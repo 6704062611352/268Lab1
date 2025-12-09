44c970e7e83693db82ace7bb159bcd23b84afaad
6704062611352 <s6704062611352@email.kmutnb.ac.th>
2025-12-09 14:52:02 +0700

    calculate VAT

--- sale.c
+++ sale.c
@@ -9,5 +9,8 @@ int main()
     printf("%.2f\n",((input1+input2)*(85))/100);
     scanf("%f",&input3);
     printf("%.2f\n",((input1+input2+input3)*(70))/100);
+    printf("%.2f\n",((input1*1.07)*95)/100);
+    printf("%.2f\n",((((input1+input2)*1.07)*(85))/100));
+    printf("%.2f\n",((((input1+input2+input3)*1.07)*(70))/100));
     return 0;
 }
